3f1fc2a053e40aa21b4be18b74334876eaaf42cc
togehter <1778267485@qq.com>
2024-01-09 15:54:21 +0800

    修改php文件

--- runoob.php
+++ runoob.php
@@ -1,4 +1,7 @@
-<?php
-echo 1;
-echo 'runoob';
-?>
+echo "# testcopo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/togetherhkl/testcopo.git
+git push -u origin main
